5145fca482abc4ed76597e09b0f5b86c564acb20
Darmimese <darmimesseleseyoum69@gmail.com>
2022-11-29 17:41:08 -0800

    queen

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,16 @@
 #!/usr/bin/node
-const square = require('./5-square');
-class Square extends square {
+
+const Square1 = require('./5-square.js');
+
+class Square extends Square1 {
   charPrint (c) {
-    if (!c) {
-      c = 'X';
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
     }
-    this.print(c);
   }
 }
 
