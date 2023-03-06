095cc6f2ca7e8bab5c83d5be72f5f4522fefd317
Darmimese <darmimesseleseyoum69@gmail.com>
2023-03-06 08:34:29 -0800

    queen

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,19 @@
 #!/usr/bin/node
+/**
+ * Script that prints the title of a Star Wars movie
+ * where the episode number matches a given integer
+ */
 const request = require('request');
-request.get('http://swapi.co/api/films/' + process.argv[2] + '/', function (err, response, body) {
-  if (err) throw err;
-  else if (response.statusCode === 200) {
-    console.log(JSON.parse(body).title);
-  }
+const process = require('process');
+
+const episodNumber = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${episodNumber}`;
+
+request.get(url, (err, _resp, body) => {
+    if (err === null) {
+        const data = JSON.parse(body);
+        console.log(data.title);
+    } else {
+        console.log(err);
+    }
 });
